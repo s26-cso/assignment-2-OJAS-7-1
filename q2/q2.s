745ae4e9f4c496036a5a0da067c5670b0516f507
OJAS-7 <ojasgdeshpande@gmail.com>
2026-04-13 16:28:09 +0530

    q2final

--- q2/q2.s
+++ q2/q2.s
@@ -1,30 +1,100 @@
-# a0 = pointer to the arr, a1 = length of the array
-# return value in a0, base address of result in a2
+.section .rodata
+fmt:    .string "%d "
+fmt1:   .string "\n"
 
 .data
-stack: .space 400     # space for 100 integers
+stack:  .space 400
 
-.text
-la s0,stack          # s0 = base address of stack
+.section .text
+.globl main
 
 main:
-    jal ra,initialise_result
-    jal ra,next_greater
+    addi sp,sp,-64
+    sd ra,56(sp)
+    sd s0,48(sp) # base address of stack
+    sd s1,40(sp) # length of arr
+    sd s2,32(sp) # base address of arr
+    sd s3,24(sp) # loop ctr
+    sd s4,16(sp) # base address of result arr
+
+    # arguements
+    addi s1,a0,-1 # s1 = lenght
+    mv s3,a1
+    addi s3,s3,8
+
+    slli a0,s1,2 # memory for input array
+    call malloc
+    mv s2,a0 # s2 = arr base address
+
+    li s4, 0 # i = 0
+
+input_loop:
+    beq s4,s1,run_logic
+    slli t1,s4,3 # offset = i*8
+    add t1,s3,t1          
+    ld a0,0(t1) # load argv string pointer
+    call atoi # convert to int
+    slli t1,s4,2 # offset = i*4
+    add t1,s2,t1          
+    sw a0,0(t1) # arr[i] = integer
+    addi s4,s4,1 # i++
+    jal x0,input_loop
+
+run_logic:
+    slli a0,s1,2 # memory for result array
+    call malloc
+    mv s4,a0 # s4 = result base address
+
+    mv a0,s2 # a0 = arr
+    mv a1,s1 # a1 = length
+    mv a2,s4 # a2 = result
+    la s0,stack # s0 = base address of stack
+
+    call initialise_result
+    mv a0,s2
+    mv a1,s1
+    mv a2,s4
+
+    call next_greater
+    li s3,0 # i =0
+
+print_loop:
+    beq s3,s1,exit
+    slli t1,s3,2 # offset = i*4
+    add t1,s4,t1          
+    lw a1,0(t1) # result[i] to be printed
+    lla a0,fmt
+    call printf
+    addi s3,s3,1 # i++
+    jal x0,print_loop
+
+exit:
+    lla a0,fmt1
+    call printf
+    
+    ld ra,56(sp)
+    ld s0,48(sp)
+    ld s1,40(sp)
+    ld s2,32(sp)
+    ld s3,24(sp)
+    ld s4,16(sp)
+    addi sp,sp,64
+    li a0,0 # return 0
     ret
 
 initialise_result: # initialise result array
-    addi t0,x0,0        # i = 0
+    addi t0,x0,0 # i = 0
 
 init_loop:
     beq t0,a1,init_done # if i == length → stop
 
-    slli t1,t0,2        # offset = i*4
-    add t1,a2,t1        # address = a2 + offset
+    slli t1,t0,2 # offset = i*4
+    add t1,a2,t1 # address = a2 + offset
 
     addi t2,x0,-1
-    sw t2,0(t1)          # result[i] = -1
+    sw t2,0(t1) # result[i] = -1
 
-    addi t0,t0,1        # i++
+    addi t0,t0,1 # i++
     jal x0,init_loop
 
 init_done:
@@ -36,21 +106,21 @@ next_greater:
     addi t2,a1,-1 # i = len - 1
     
 loop:
-    beq t2,t3,end_loop
-    beq t0,t3,push_next # if stack empty,push
+    bltz t2,end_loop # stop loop if i < 0
+    beq t0,t3,push_next # if stack empty, push
 
     slli t6,t2,2      
     add t6,a0,t6
-    lw t4,0(t6) # arr[i]
+    lw t4,0(t6)  # arr[i]
 
-    add t7,s0,t0 
-    lw t7,0(t7) # stack.top[top]
+    add t6,s0,t0 # stack.top[top]
+    lw t6,0(t6) 
 
-    slli t6,t7,2
+    slli t6,t6,2
     add t6,a0,t6
     lw t5,0(t6) # arr[stack[top]]
 
-    bge t4,t5,pop # if not empty and arr[i]>=arr[stack.top()]
+    bge t4,t5,pop # if not empty and arr[i] >= arr[stack.top()]
     
     slli t6,t2,2
     add t6,a2,t6
@@ -69,7 +139,6 @@ push_next:
     addi t2,t2,-1 # next iteration
     jal x0,loop
 
-pop:
+pop:k
     addi t0,t0,-4 # top--
     jal x0,loop
-
